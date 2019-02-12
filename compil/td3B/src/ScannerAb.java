73534c1f1ed9465b6ad4c31402114e9a97508297
Thierno Sambegou Diallo <tsdiallo001@emi.u-bordeaux1.fr>
2019-02-12 20:57:00 +0100

    java

---
+++ compil/td3B/src/ScannerAb.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import beaver.Scanner;
+import beaver.Symbol;
+
+public class ScannerAb extends Scanner {
+
+	private InputStreamReader reader;
+
+	@Override
+	public Symbol nextToken() throws IOException, Exception {
+		for (;;) {
+			switch (reader.read()) {
+			case 'a':
+				return new Symbol(Terminals.A);
+			case 'b':
+				return new Symbol(Terminals.B);
+			case '\n':
+				return new Symbol(Terminals.NEWLINE);
+			case -1:
+				return new Symbol(Terminals.EOF);
+			default:
+				break;
+			}
+		}
+	}
+
+	public ScannerAb(InputStreamReader reader) {
+		super();
+		this.reader = reader;
+	}
+
+}
